3295392cbdacfb917ced7d8c1cf608a7d971e75d
Yergazy Nur <yergazy0486@yahoo.com>
2021-06-16 03:19:26 +0600

    add new files

---
+++ AmOnPage_test.js
@@ -0,0 +1,33 @@
+Feature('GoogleSearch');
+
+Scenario('I.amOnPage-Test', ({ I }) => {
+    I.amOnPage('/');
+    I.wait(1);
+
+    I.amOnPage('/doodles');
+    I.wait(1);
+
+    I.amOnPage('https://codecept.io');
+    I.wait(1);
+
+    I.amOnPage('https://codecept.io/playwright/#setup');
+    I.wait(1);
+
+    });
+
+    Scenario('2.TestCase2-Test', ({I}) => {
+        
+        I.amOnPage('https://github.com/');
+        I.wait(1);
+
+        I.amOnPage('https://github.com/login');
+        I.wait(1);
+
+        I.see("Sign in to GitHub");
+
+        I.fillField("#login_field", "YergazyN");
+        I.fillField("#password", "Aikosha2017");
+        I.click(".btn");
+        I.see("Repositories ");
+        I.dontSee("Sign in to GitHub");
+    });
